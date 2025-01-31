7011fe3f14d14550f57ad14f52a6ec5af44fd2c4
fujino <fko2447032@stu.o-hara.ac.jp>
2025-01-31 10:30:00 +0900

    first commit

---
+++ scheduleapp/views.py
@@ -0,0 +1,110 @@
+import datetime
+from django.shortcuts import render,redirect
+from django.views.generic.base import TemplateView
+from django.views import generic
+from . import mixins
+from .models import Schedule
+from .forms import BS4ScheduleForm,SimpleScheduleForm
+
+
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'month.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
+
+class WeekCalendar(mixins.WeekCalendarMixin, generic.TemplateView):
+    """週間カレンダーを表示するビュー"""
+    template_name = 'week.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_week_calendar()
+        context.update(calendar_context)
+        return context
+
+class WeekWithScheduleCalendar(mixins.WeekWithScheduleMixin, generic.TemplateView):
+    """スケジュール付きの週間カレンダーを表示するビュー"""
+    template_name = 'week_with_schedule.html'
+    model = Schedule
+    date_field = 'date'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_week_calendar()
+        context.update(calendar_context)
+        context['week_row'] = zip(
+            calendar_context['week_names'],
+            calendar_context['week_days'],
+            calendar_context['week_day_schedules'].values(),
+        )
+        return context
+
+class MonthWithScheduleCalendar(mixins.MonthWithScheduleMixin, generic.TemplateView):
+    """スケジュール付きの月間カレンダーを表示するビュー"""
+    template_name = 'month_with_schedule.html'
+    model = Schedule
+    date_field = 'date'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
+
+class MyCalendar(mixins.MonthCalendarMixin, mixins.WeekWithScheduleMixin, generic.CreateView):
+    """月間カレンダー、週間カレンダー、スケジュール登録画面のある欲張りビュー"""
+    template_name = 'mycalendar.html'
+    model = Schedule
+    date_field = 'date'
+    form_class = BS4ScheduleForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        week_calendar_context = self.get_week_calendar()
+        month_calendar_context = self.get_month_calendar()
+        context.update(week_calendar_context)
+        context.update(month_calendar_context)
+        return context
+
+    def form_valid(self, form):
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        day = self.kwargs.get('day')
+        if month and year and day:
+            date = datetime.date(year=int(year), month=int(month), day=int(day))
+        else:
+            date = datetime.date.today()
+        schedule = form.save(commit=False)
+        schedule.date = date
+        schedule.save()
+        return redirect('scheduleapp:mycalendar', year=date.year, month=date.month, day=date.day)
+
+class MonthWithFormsCalendar(mixins.MonthWithFormsMixin, generic.View):
+    """フォーム付きの月間カレンダーを表示するビュー"""
+    template_name = 'month_with_forms.html'
+    model = Schedule
+    date_field = 'date'
+    form_class = SimpleScheduleForm
+
+    def get(self, request, **kwargs):
+        context = self.get_month_calendar()
+        return render(request, self.template_name, context)
+
+    def post(self, request, **kwargs):
+        context = self.get_month_calendar()
+        formset = context['month_formset']
+        if formset.is_valid():
+            formset.save()
+            return redirect('scheduleapp:month_with_forms')
+
+        return render(request, self.template_name, context)
